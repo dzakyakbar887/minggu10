448ee198becd05c528cc1bbb592664aa3891eb2b
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-05-12 18:55:18 +0700

    endif anak

--- anak.h
+++ anak.h
@@ -4,4 +4,14 @@
 #include <iostream>
 using namespace std;
 
-class anak 
+class anak {
+public:
+    string nama;
+    anak(string pNama) :nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
